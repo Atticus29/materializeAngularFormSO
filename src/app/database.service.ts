a9af9558903eddd327a123dbd001bf7f267e6301
Mark <mark.aaron.fisher@gmail.com>
2017-12-19 12:47:05 -0800

    create database service and confirm successful push of data to it

---
+++ src/app/database.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/combineLatest';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/scan';
+import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import * as firebase from 'firebase/app';
+
+@Injectable()
+export class DatabaseService {
+  opts: FirebaseListObservable<any>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.opts = db.list('/options');
+  }
+
+  addOptToDb(opt: string){
+    this.opts.push(opt);
+  }
+
+}
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, FormArray, Validators} from '@angular/forms';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { DatabaseService } from '../database.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
+  providers: [DatabaseService]
 })
 export class FormComponent implements OnInit {
   testForm: FormGroup;
   opts: Array<string> = ["option1", "option2", "option3"];
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private db: DatabaseService) { }
 
   ngOnInit() {
+    let dbOpts: Array<string> = ["opt1 from firebase", "opt2 from firebase", "opt2 from firebase"];
+
+    //####################################################
+    //#####Did this one time to populate the database#####
+    //####################################################
+    // dbOpts.forEach(option=>{
+    //   this.db.addOptToDb(option);
+    // });
+
     this.testForm = this.fb.group({
 
     })
