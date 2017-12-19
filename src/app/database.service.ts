5dd5b4ce19e34efadd757d08d70398b2973f0553
Mark <mark.aaron.fisher@gmail.com>
2017-12-19 13:07:17 -0800

    get the option tag issue reproduced

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -19,4 +19,8 @@ export class DatabaseService {
     this.opts.push(opt);
   }
 
+  getOpts(){
+    return this.opts;
+  }
+
 }
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, FormArray, Validators} from '@angular/forms';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { DatabaseService } from '../database.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-form',
@@ -10,24 +11,30 @@ import { DatabaseService } from '../database.service';
   providers: [DatabaseService]
 })
 export class FormComponent implements OnInit {
+  private ngUnsubscribe: Subject<void> = new Subject<void>();
   testForm: FormGroup;
   opts: Array<string> = ["option1", "option2", "option3"];
+  retrievedDbOpts: any[];
 
   constructor(private fb: FormBuilder, private db: DatabaseService) { }
 
   ngOnInit() {
-    let dbOpts: Array<string> = ["opt1 from firebase", "opt2 from firebase", "opt2 from firebase"];
 
     //####################################################
     //#####Did this one time to populate the database#####
     //####################################################
+    // let dbOpts: Array<string> = ["opt1 from firebase", "opt2 from firebase", "opt2 from firebase"];
     // dbOpts.forEach(option=>{
     //   this.db.addOptToDb(option);
     // });
 
     this.testForm = this.fb.group({
 
-    })
+    });
+
+    this.db.getOpts().subscribe(opts=>{
+      this.retrievedDbOpts = opts;
+    });
   }
 
 }
